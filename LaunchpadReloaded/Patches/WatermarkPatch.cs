c6cec205eb76008abf9dce61a877dd856a19f7fc
ang-xd <angxdxdd@gmail.com>
2024-03-01 22:26:02 -0500

    Watermark

---
+++ LaunchpadReloaded/Patches/WatermarkPatch.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using Reactor.Utilities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+namespace LaunchpadReloaded.Patches;
+
+[HarmonyPatch(typeof(PingTracker))]
+public static class WatermarkPatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(PingTracker.Update))]
+    public static bool Prefix(PingTracker __instance)
+    {
+        AspectPosition aspectPos = __instance.GetComponent<AspectPosition>();
+
+        __instance.gameObject.SetActive(true);
+        __instance.text.richText = true;
+        __instance.text.text = "<color=#FF4050FF>All Of Us:</color> Launchpad \n<color=#7785CC>dsc.gg/allofus</color>";
+
+        aspectPos.DistanceFromEdge = new Vector3(4, 0.1f, 0);
+
+        return false;
+    }
+}
