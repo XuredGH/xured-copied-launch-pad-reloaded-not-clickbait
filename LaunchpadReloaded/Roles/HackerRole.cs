b13ec440718d1adf34c7ad9a6fc5c29a6a3fe5a9
XtraCube <uiqbal544@gmail.com>
2024-03-06 20:49:03 -0500

    consistency in roles

--- LaunchpadReloaded/Roles/HackerRole.cs
+++ LaunchpadReloaded/Roles/HackerRole.cs
@@ -1,6 +1,6 @@
-﻿using LaunchpadReloaded.API.Roles;
+﻿using System;
+using LaunchpadReloaded.API.Roles;
 using Reactor.Utilities.Attributes;
-using System;
 using UnityEngine;
 
 namespace LaunchpadReloaded.Roles;
@@ -13,4 +13,5 @@ public class HackerRole(IntPtr ptr) : ImpostorRole(ptr), ICustomRole
     public string RoleLongDescription => "The hacker can hack meetings which causes everyone to go anonymous, and can perform an advanced comms sabotage which causes everyone to go anonymous in game.";
     public Color RoleColor => new Color32(20, 148, 20, 255);
     public RoleTeamTypes Team => RoleTeamTypes.Impostor;
+    public override bool IsDead => false;
 }
--- LaunchpadReloaded/Roles/JanitorRole.cs
+++ LaunchpadReloaded/Roles/JanitorRole.cs
@@ -13,5 +13,6 @@ public class JanitorRole(IntPtr ptr) : ImpostorRole(ptr), ICustomRole
     public string RoleLongDescription => "The Janitor is an impostor with the ability to drag bodies! The Janitor can stash bodies in vents, disabling them in the process! Cleaning vents will expose the body!";
     public Color RoleColor => Color.yellow;
     public RoleTeamTypes Team => RoleTeamTypes.Impostor;
+    public override bool IsDead => false;
     public bool TargetsBodies => true;
 }
