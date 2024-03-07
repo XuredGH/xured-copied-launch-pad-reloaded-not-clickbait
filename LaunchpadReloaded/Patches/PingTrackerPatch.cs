6c456867f37522cf8fb199786019e79037495768
XtraCube <uiqbal544@gmail.com>
2024-03-06 20:43:10 -0500

    redo pingtracker to angxls liking

--- LaunchpadReloaded/Patches/PingTrackerPatch.cs
+++ LaunchpadReloaded/Patches/PingTrackerPatch.cs
@@ -10,6 +10,12 @@ public static class PingTrackerPatch
     [HarmonyPatch(nameof(PingTracker.Update))]
     public static void Postfix(PingTracker __instance)
     {
-        __instance.text.text += "\n<align=\"center\"><color=#FF4050FF>All Of Us:</color> Launchpad \n<color=#7785CC>dsc.gg/allofus</color>";
+        __instance.gameObject.SetActive(true);
+        __instance.text.text =
+            $"<align=\"center\"><color=#FF4050FF>All Of Us:</color> Launchpad \n<size=90%><color=#7785CC>dsc.gg/allofus</color>\n<size=80%>{__instance.text.text}";
+        
+        var aspectPos = __instance.GetComponent<AspectPosition>();
+        var x = HudManager.Instance.GetComponentInChildren<FriendsListButton>() ? 3.5f : 2f;
+        aspectPos.DistanceFromEdge = new Vector3(x, 0.1f, 0);
     }
 }
