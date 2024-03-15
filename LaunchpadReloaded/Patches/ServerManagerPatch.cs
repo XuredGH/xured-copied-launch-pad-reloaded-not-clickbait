af0bcace164f4c3fe84b9dd4b7dd735cd4bb394a
XtraCube <uiqbal544@gmail.com>
2024-03-15 08:30:37 -0400

    add launchpad region

---
+++ LaunchpadReloaded/Patches/ServerManagerPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using Reactor.Localization.Utilities;
+
+namespace LaunchpadReloaded.Patches;
+
+[HarmonyPatch(typeof(ServerManager),nameof(ServerManager.LoadServers))]
+public static class ServerManagerPatch
+{
+    public static void Postfix(ServerManager __instance)
+    {
+        var serverInfo = new ServerInfo("http-1", "http://brand-lauderdale.gl.at.ply.gg", 7764, false);
+        ServerInfo[] arr = [serverInfo];
+        var regionInfo = new StaticHttpRegionInfo("launchpad test server", (StringNames)1003,"brand-lauderdale.gl.at.ply.gg", arr);
+        
+        __instance.AddOrUpdateRegion(regionInfo.Cast<IRegionInfo>());
+        
+        
+    }
+}
