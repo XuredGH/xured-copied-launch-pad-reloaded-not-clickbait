33e0ff42d042848033f2eb8ca5511c7835821cb3
ang-xd <angxdxdd@gmail.com>
2024-03-16 13:32:21 -0400

    Server patch

--- LaunchpadReloaded/Patches/ServerManagerPatch.cs
+++ LaunchpadReloaded/Patches/ServerManagerPatch.cs
@@ -1,19 +1,16 @@
-﻿/*using HarmonyLib;
-using Reactor.Localization.Utilities;
+﻿using HarmonyLib;
 
 namespace LaunchpadReloaded.Patches;
 
-[HarmonyPatch(typeof(ServerManager),nameof(ServerManager.LoadServers))]
+[HarmonyPatch(typeof(ServerManager), nameof(ServerManager.LoadServers))]
 public static class ServerManagerPatch
 {
     public static void Postfix(ServerManager __instance)
     {
-        var serverInfo = new ServerInfo("http-1", "http://"+LaunchpadReloadedPlugin.LaunchpadServerAddress, LaunchpadReloadedPlugin.LaunchpadServerPort, false);
+        var serverInfo = new ServerInfo("http-1", "http://" + LaunchpadReloadedPlugin.LaunchpadServerAddress, LaunchpadReloadedPlugin.LaunchpadServerPort, false);
         ServerInfo[] arr = [serverInfo];
-        var regionInfo = new StaticHttpRegionInfo("launchpad test server", (StringNames)1003,LaunchpadReloadedPlugin.LaunchpadServerAddress, arr);
-        
+        var regionInfo = new StaticHttpRegionInfo("Launchpad Beta Testing", (StringNames)1003, LaunchpadReloadedPlugin.LaunchpadServerAddress, arr);
+
         __instance.AddOrUpdateRegion(regionInfo.Cast<IRegionInfo>());
-        
-        
     }
-}*/
+}
