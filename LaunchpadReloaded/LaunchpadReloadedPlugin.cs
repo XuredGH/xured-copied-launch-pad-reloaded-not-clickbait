fe25915afb137a9c1437819c2bc1e72349de1c52
XtraCube <uiqbal544@gmail.com>
2024-04-01 11:27:31 -0400

    remove ServerManagerPatch.cs

--- LaunchpadReloaded/LaunchpadReloadedPlugin.cs
+++ LaunchpadReloaded/LaunchpadReloadedPlugin.cs
@@ -26,9 +26,6 @@ public partial class LaunchpadReloadedPlugin : BasePlugin
     public Harmony Harmony { get; } = new(Id);
     public static LaunchpadReloadedPlugin Instance { get; private set; }
 
-    public const string LaunchpadServerAddress = "brand-lauderdale.gl.at.ply.gg";
-    public const ushort LaunchpadServerPort = 7764;
-
     public override void Load()
     {
         Instance = this;
