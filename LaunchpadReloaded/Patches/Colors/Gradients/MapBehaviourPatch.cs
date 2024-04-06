fd438f4a2c5d2a36d34a1faf44961634ad61998a
XtraCube <uiqbal544@gmail.com>
2024-04-05 21:38:15 -0400

    wip untested map icon fix

---
+++ LaunchpadReloaded/Patches/Colors/Gradients/MapBehaviourPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using LaunchpadReloaded.Components;
+using LaunchpadReloaded.Features;
+using LaunchpadReloaded.Utilities;
+
+namespace LaunchpadReloaded.Patches.Colors.Gradients;
+
+[HarmonyPatch(typeof(MapBehaviour),nameof(MapBehaviour.Awake))]
+public static class MapBehaviourPatch
+{
+    public static void Postfix(MapBehaviour __instance)
+    {
+        __instance.HerePoint.material = LaunchpadAssets.GradientMaterial.LoadAsset();
+        
+        __instance.HerePoint.gameObject.AddComponent<GradientColorComponent>().SetColor(PlayerControl.LocalPlayer.Data.DefaultOutfit.ColorId, PlayerControl.LocalPlayer.GetComponent<PlayerGradientData>().GradientColor);
+
+        var mat = __instance.HerePoint.material;
+        
+        mat.SetFloat(ShaderID.GradientBlend, 5);
+        mat.SetFloat(ShaderID.GradientOffset, .35f);
+        
+    }
+}
