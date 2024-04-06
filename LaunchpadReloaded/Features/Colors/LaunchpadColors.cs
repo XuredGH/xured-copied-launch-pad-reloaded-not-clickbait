553a6722b9458b972f18e825dc87d4c06966ccd3
XtraCube <uiqbal544@gmail.com>
2024-04-05 21:39:06 -0400

    remove similar colors

--- LaunchpadReloaded/Features/Colors/LaunchpadColors.cs
+++ LaunchpadReloaded/Features/Colors/LaunchpadColors.cs
@@ -11,10 +11,5 @@ public static class LaunchpadColors
     public static CustomColor Lavender => new(new Color32(181, 176, 255, 255), "Lavender");
     public static CustomColor Iris => new(new Color32(90, 79, 207, 255), "Iris");
     public static CustomColor Viridian => new(new Color32(64, 130, 109, 255), "Viridian");
-    public static CustomColor Terracotta => new(new Color32(226, 114, 91, 255), "Terracotta");
-    public static CustomColor Cherry => new(new Color32(59, 1, 0, 255), "Cherry");
-    public static CustomColor Azure => new(new Color32(0, 233, 255, 255), "Azure");
-    public static CustomColor Faschia => new(new Color32(192, 32, 64, 255), "Faschia");
-    public static CustomColor Mahogany => new(new Color32(173, 109, 104, 255), "Mahogany");
-    public static CustomColor Blurple => new(new Color32(77, 50, 194, 255), new Color32(59, 38, 149, 255), "Blurple");
+    public static CustomColor Blurple => new(new Color32(114, 137, 218, 255), new Color32(80, 96, 153, 255), "Blurple");
 }
