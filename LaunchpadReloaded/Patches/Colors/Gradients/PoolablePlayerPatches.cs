b43665d8c3d8436e8c8e248952578e8bf0aa8c27
XtraCube <uiqbal544@gmail.com>
2024-04-05 21:38:32 -0400

    add offsets and blend for poolable players

---
+++ LaunchpadReloaded/Patches/Colors/Gradients/PoolablePlayerPatches.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using LaunchpadReloaded.Utilities;
+
+namespace LaunchpadReloaded.Patches.Colors.Gradients;
+
+[HarmonyPatch(typeof(PoolablePlayer))]
+public static class PoolablePlayerPatches
+{
+    [HarmonyPatch(nameof(PoolablePlayer.UpdateFromPlayerData))]
+    [HarmonyPatch(nameof(PoolablePlayer.UpdateFromEitherPlayerDataOrCache))]
+    public static void UpdateFromPlayerPrefix(PoolablePlayer __instance, [HarmonyArgument(0)] GameData.PlayerInfo playerInfo)
+    {
+        __instance.gameObject.SetGradientData(playerInfo.PlayerId);
+        var mat = __instance.cosmetics.currentBodySprite.BodySprite.material;
+        
+        mat.SetFloat(ShaderID.GradientBlend, 3f);
+        mat.SetFloat(ShaderID.GradientOffset, .4f);
+    }
+}
