52cf862d14290b105375c88917ed69b18fa7320c
Xured <oofderlol0@gmail.com>
2025-04-07 08:10:12 +1000

    baby and humongous

---
+++ LaunchpadReloaded/Modifiers/Game/BabyModifer.cs
@@ -0,0 +1,31 @@
+﻿using LaunchpadReloaded.Options.Modifiers;
+using MiraAPI.GameOptions;
+using MiraAPI.Modifiers;
+
+namespace LaunchpadReloaded.Modifiers.Fun;
+
+public sealed class BabyModifier : LPModifier
+{
+    public override string ModifierName => "Baby";
+    public override int GetAssignmentChance() => (int)OptionGroupSingleton<GameModifierOptions>.Instance.SmolChance;
+    public override int GetAmountPerGame() => 1;
+    public override bool IsModifierValidOn(RoleBehaviour role) => base.IsModifierValidOn(role) && !role.Player.HasModifier<GiantModifier>();
+
+    public override void OnActivate()
+    {
+        if (Player != null)
+        {
+            Player.MyPhysics.Speed /= 0.5f;
+            Player.transform.localScale *= 0.3f;
+        }
+    }
+
+    public override void OnDeactivate()
+    {
+        if (Player != null)
+        {
+            Player.MyPhysics.Speed *= 0.5f;
+            Player.transform.localScale /= 0.3f;
+        }
+    }
+}
---
+++ LaunchpadReloaded/Modifiers/Game/HumongousModifer.cs
@@ -0,0 +1,30 @@
+﻿using LaunchpadReloaded.Options.Modifiers;
+using MiraAPI.GameOptions;
+using MiraAPI.Modifiers;
+
+namespace LaunchpadReloaded.Modifiers.Fun;
+
+public sealed class HumongousModifier : LPModifier
+{
+    public override string ModifierName => "Humongous";
+    public override int GetAssignmentChance() => (int)OptionGroupSingleton<GameModifierOptions>.Instance.GiantChance;
+    public override int GetAmountPerGame() => 1;
+    public override bool IsModifierValidOn(RoleBehaviour role) => base.IsModifierValidOn(role) && !role.Player.HasModifier<SmolModifier>();
+    public override void OnActivate()
+    {
+        if (Player != null)
+        {
+            Player.MyPhysics.Speed *= 0.5f;
+            Player.transform.localScale /= 0.5f;
+        }
+    }
+
+    public override void OnDeactivate()
+    {
+        if (Player != null)
+        {
+            Player.MyPhysics.Speed /= 0.5f;
+            Player.transform.localScale *= 0.5f;
+        }
+    }
+}
