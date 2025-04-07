37c2117e1d0112610053c6f403ed7c00fe49e4f7
Xured <oofderlol0@gmail.com>
2025-04-07 12:35:18 +1000

    Torch Modifier

---
+++ LaunchpadReloaded/Modifiers/Game/TorchModifier.cs
@@ -0,0 +1,13 @@
+﻿using LaunchpadReloaded.Options.Modifiers;
+ using LaunchpadReloaded.Options.Modifiers.Crewmate;
+ using MiraAPI.GameOptions;
+ using MiraAPI.Utilities;
+ 
+ namespace LaunchpadReloaded.Modifiers;
+ 
+ public sealed class TorchModifier : LPModifier
+ {
+     public override string ModifierName => "Torch";
+     public override int GetAssignmentChance() => (int)OptionGroupSingleton<CrewmateModifierOptions>.Instance.TorchChance;
+     public override int GetAmountPerGame() => 1;
+ }
