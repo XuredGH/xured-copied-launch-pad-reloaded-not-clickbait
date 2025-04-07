250a79a121a8472246b5af26d5b69d9ab752eea1
Xured <oofderlol0@gmail.com>
2025-04-07 12:31:02 +1000

    Vendetta + Executioner

---
+++ LaunchpadReloaded/Modifiers/VendettaMarkModifier.cs
@@ -0,0 +1,15 @@
+﻿using MiraAPI.Modifiers;
+ 
+ namespace LaunchpadReloaded.Modifiers;
+ 
+ public class VendettaMarkModifier(byte vendettaPlayer) : BaseModifier
+ {
+     public override string ModifierName => "Vendetta";
+     public override bool HideOnUi => true;
+     public PlayerControl Vendetta { get; private set; } = GameData.Instance.GetPlayerById(vendettaPlayer).Object;
+ 
+     public override void OnDeath(DeathReason reason)
+     {
+         ModifierComponent!.RemoveModifier(this);
+     }
+ }
